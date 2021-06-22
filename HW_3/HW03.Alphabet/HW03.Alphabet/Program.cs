27c48ed87875fca7677f4d52816ab49ae4266ddb
AliokY <aliokhinyury@gmail.com>
2021-06-22 21:31:15 +0300

    alphabet in reverse

---
+++ HW_3/HW03.Alphabet/HW03.Alphabet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW03.Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 122; 97 <= i; i--)
+            {
+                Console.Write(Convert.ToChar(i) + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
