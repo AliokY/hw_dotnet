f4d76c253192eb18fb8065b24b0ba1fac16f71d3
AliokY <aliokhinyury@gmail.com>
2021-06-26 00:59:00 +0300

    add task4

---
+++ HW_4/HW04/HW04.Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW04.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стихотворение в одну строку, разделяя строки точкой с запятой");
+
+            string verse = Console.ReadLine();
+
+            string[] linesVerse = verse.Split(new char[] { ';' });
+
+            for (int i = 0; i < linesVerse.Length; i++)
+            {
+                linesVerse[i] = linesVerse[i].Replace("о", "а");
+                linesVerse[i] = linesVerse[i].Replace("О", "А");
+            }
+
+            foreach (string line in linesVerse)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadKey();
+        }
+    }
+}
