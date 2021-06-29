9f8d0eea8f16fe65b9b419a2b049810c37c1fa7d
AliokY <aliokhinyury@gmail.com>
2021-06-29 11:12:47 +0300

    add .gitignore

--- HW_3/HW03.Alphabet/HW03.Alphabet/Program.cs
+++ HW_3/HW03.Alphabet/HW03.Alphabet/Program.cs
@@ -4,11 +4,11 @@ namespace HW03.Alphabet
 {
     class Program
     {
-        int decEncodingLowercaseA = 97;
-        int decEncodingLowercaseZ = 122;
-
         static void Main(string[] args)
         {
+            int decEncodingLowercaseA = 97;
+            int decEncodingLowercaseZ = 122;
+
             for (int i = decEncodingLowercaseZ; decEncodingLowercaseA <= i; i--)
             {
                 Console.Write(Convert.ToChar(i) + " ");
