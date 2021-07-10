f6ff8d4820c70ab2c6feea89d0fdc3dc6b18a011
Yury Aliokhin <aliokhinyury@gmail.com>
2021-07-10 23:45:37 +0300

    part of Task3

---
+++ HW_7/HW07/Task3/EnyFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    abstract class EnyFile
+    {
+        public abstract string name { get; set; }
+
+        public abstract Guid id { get; set; }
+
+        public abstract FileType fileType { get; set; }
+
+        public abstract int fileSize { get; set; }
+
+        public EnyFile(string name, FileType fileType, int fileSize)
+        {
+            id = Guid.NewGuid();
+
+            this.name = name;
+            this.fileType =fileType;
+            this.fileSize = fileSize;
+        }
+
+        public enum FileType
+        {
+            Music = 1,
+            Movie,
+            Program
+        }
+    }
+}
